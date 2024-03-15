6aebc5d9d400fc9a5ca8d511218e3f3bd04ae932
Gustavo Kesley Fontes <72459288+gustavo-f0ntz@users.noreply.github.com>
2024-03-15 15:19:24 -0300

    feat: Add 'addMedico' and 'listMedico' functions to medicos.c
    Added the 'addMedico' and 'listMedico' functions to medicos.c for managing doctors.

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
-#include "medicos.c"
-#include "pacientes.c"
+#include "medicos.h"
+#include "pacientes.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 int main(void){
-
+    Medico *medico1 = addMedico("Dr. Joao", "Cardiologista", "Maria", "Segunda-feira");
+    Medico *medico2 = addMedico("Dr. Pedro", "Ortopedista", "Jose", "Terca-feira");
+    listMedicos(medico1);
+    listMedicos(medico2);
     return 0;
 }
--- medicos.c
+++ medicos.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "medicos.h"
+#include <string.h>
 
 struct medico
 {
@@ -9,3 +10,27 @@ struct medico
     char paciente[50];
     char disponibilidade[50];
 };
+
+Medico *addMedico(char *nome, char *especialidade, char *paciente, char *disponibilidade)
+{
+    Medico *medico = (Medico *)malloc(sizeof(Medico));
+    if (medico == NULL)
+    {
+        printf("Memoria insuficiente \n");
+        exit(1);
+    }
+    strcpy(medico->nome, nome);
+    strcpy(medico->especialidade, especialidade);
+    strcpy(medico->paciente, paciente);
+    strcpy(medico->disponibilidade, disponibilidade);
+    return medico;
+}
+
+void listMedicos(Medico *medico)
+{
+    printf("Nome: %s\n", medico->nome);
+    printf("Especialidade: %s\n", medico->especialidade);
+    printf("Paciente: %s\n", medico->paciente);
+    printf("Disponibilidade: %s\n", medico->disponibilidade);
+}
+
